93d96c678ee7d53b9710ba85f5ce572bcbce6d2f
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-06-24 00:30:06 +0700

    validator negara dan nomor

--- App.js
+++ App.js
@@ -1,1 +1,8 @@
 console.log('halo');
+
+const validator = require('validator');
+
+console.log(validator.isEmail('A@gmail.com'));
+console.log(validator.isMobilePhone('085830379', 'id-ID'));
+console.log(validator.isNumeric('085830379'));
+
